6e51e5495dfb6dd913289e953dbbed863de9c68a
deepesh rana <deepeshranaarron@gmail.com>
2025-08-24 00:34:53 +0530

    Change

--- Java/src/in/learn/java/Fundamental.java
+++ Java/src/in/learn/java/Fundamental.java
@@ -5,6 +5,7 @@ public class Fundamental {
 	public static void main(String[] args) {
 		System.out.println("Hello how are you?");
 		System.out.println("Good fine!");
+		System.out.println("Good!");
 	}
 
 }
