682fc6d40b0e30eb32bdaddfafa89bfbc26f4a0e
deepesh rana <deepeshranaarron@gmail.com>
2025-08-24 00:32:53 +0530

    Hello code

--- Java/src/in/learn/java/Fundamental.java
+++ Java/src/in/learn/java/Fundamental.java
@@ -1,5 +1,10 @@
 package in.learn.java;
 
 public class Fundamental {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello how are you?");
+		System.out.println("Good fine!");
+	}
 
 }
